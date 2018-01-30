b498dc41ec3735282c9415ec0c581706ad01840a
RomanNikonovich <33447044+RomanNikonovich@users.noreply.github.com>
2018-01-30 22:04:51 +0300

    Roman nikonovich (#2)
    * new chenge
    
    * r1
    
    * d1

--- src/test/Main.java
+++ src/test/Main.java
@@ -4,15 +4,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
         test();
+        test2();
     }
 
     private static void test() {
-
+        System.out.println("Some chenge");
     }
 
     private static void test2() {
         System.out.println("I'm here");
         System.out.println("I'm here anyway");
+
+
+    }
+    private  static void test3(){
+        System.out.println("ky ky ....");
 
     }
 }
