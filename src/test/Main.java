3333ea7af74f7ee86e7e778f62413766901f3e11
RomanNikonovich <33447044+RomanNikonovich@users.noreply.github.com>
2018-01-30 21:43:56 +0300

    r1

--- src/test/Main.java
+++ src/test/Main.java
@@ -4,10 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
         test();
+        test2();
     }
 
     private static void test() {
-
+        System.out.println("Some chenge");
     }
 
     private static void test2() {
